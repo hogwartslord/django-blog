8d7b97e09213144a6596350e4b7baad5fd8cc6b7
hogwartslord <1040926265@qq.com>
2017-11-07 09:43:16 +0800

    2017 11 07 redirect

--- demo/blog/views.py
+++ demo/blog/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect,HttpResponsePermanentRedirect
 
 # Create your views here.
 
 def index(request):
-    return render(request,'blog/index.html')
+    context = {'title':'test'}
+    return render(request,'blog/index.html',context=context)
 
 def hello(request):
     return HttpResponse('Hello World!')
 
-def add(request,num1,num2):
-    num1 = int(num1)
-    num2 = int(num2)
-    ret = '{} + {} = {}'.format(num1,num2,num1+num2)
+def add(request,n1,n2):
+    n1 = int(n1)
+    n2 = int(n2)
+    ret = '{} + {} = {}'.format(n1,n2,n1+n2)
     return HttpResponse(ret)
 
+def mul(request):
+    n1 = int(request.GET.get('n1'))
+    n2 = int(request.GET.get('n2'))
+    ret = '{} * {} = {}'.format(n1, n2, n1 * n2)
+    return HttpResponse(ret)
 
-
+def addtest(request):
+    return HttpResponseRedirect('/blog/600/66/')
