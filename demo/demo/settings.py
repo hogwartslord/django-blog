2a61b398e368fc7451d22ec9bbb5224035c59dc5
hogwartslord <1040926265@qq.com>
2017-11-20 16:57:14 +0800

    2017-11-20

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -123,6 +123,15 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# 当运行 python manage.py collectstatic 的时候
+# STATIC_ROOT 文件夹 是用来将所有STATICFILES_DIRS中所有文件夹中的文件，以及各app中static中的文件都复制过来
+# 把这些文件放到一起是为了用apache等部署的时候更方便
+STATIC_ROOT = os.path.join(BASE_DIR,'app_static')
+# 其它 存放静态文件的文件夹，可以用来存放项目中公用的静态文件，里面不能包含 STATIC_ROOT
+# 如果不想用 STATICFILES_DIRS 可以不用，都放在 app 里的 static 中也可以
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 
 '''
 LOGGING = {
